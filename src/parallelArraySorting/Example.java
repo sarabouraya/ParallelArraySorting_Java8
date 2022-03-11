e8d97d9e009ef0265427dd44cf643f49b935803b
SBouraya <80527603+SBouraya@users.noreply.github.com>
2022-03-11 21:12:01 +0100

    first commit

---
+++ src/parallelArraySorting/Example.java
@@ -0,0 +1,24 @@
+package parallelArraySorting;
+
+import java.util.Arrays;
+
+public class Example {
+
+	public static void main(String[] args) {
+		
+		int[] myArray = {5,9,1,8,2,7,3,6,4};
+		
+		for (int i = 0; i < myArray.length; i++) {
+			System.out.print(" "+myArray[i]);
+		}
+		
+		Arrays.parallelSort(myArray);
+		
+		System.out.println();
+		
+		for (int i = 0; i < myArray.length; i++) {
+			System.out.print(" "+myArray[i]);
+		}
+		
+	}
+}
